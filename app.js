818f6079741b3d3e265723025f69007464f21e51
MrGray17 <hammoubelyazid@gmail.com>
2026-06-23 14:55:04 +0100

    further improvments

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-let transactions = [];
+let transactions = JSON.parse(localStorage.getItem("myExpanses")) || [];
 
 const expenseInp = document.getElementById("bght-btn");
 const amountInp = document.getElementById("cost-btn");
 const submBtn = document.getElementById("submit");
+const list = document.getElementById("expense-list");
+const clr = document.getElementById("clear");
 
 const getUserInfos = async () => {
     let userBght = expenseInp.value ;
@@ -16,5 +18,14 @@ const getUserInfos = async () => {
     stringTrans = JSON.stringify(transactions);
     localStorage.setItem("myExpanses" , stringTrans);
     console.log(userBght, userInp);
+    
+    let li = document.createElement("li");
+    li.textContent = `${userBght} - $${userInp}` ;
+    list.appendChild(li);
+}
+
+const ClearBtn = () => {
+    list.textContent = "";
 }
 submBtn.addEventListener("click" , getUserInfos);
+clr.addEventListener("click" , ClearBtn);
